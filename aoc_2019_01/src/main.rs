f2a765f9b0421ccd2406506f20add909714b0717
Jake Cleary <shout@jakecleary.net>
2019-12-02 14:48:08 +0000

    [1] Complete puzzle 01:02

--- aoc_2019_01/src/main.rs
+++ aoc_2019_01/src/main.rs
@@ -1,16 +1,38 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
+use std::cmp;
 
 fn main() {
     let file = File::open("input.txt").expect("Something went wrong!");
     let reader = BufReader::new(file);
     let mut total_fuel_required = 0;
 
-    for line in reader.lines() {
-        let weight = line.expect("Something went wrong!").parse::<i32>().unwrap();
-        let fuel_required = ((weight / 3) as i64) - 2;
-        total_fuel_required += fuel_required;
+    for module in reader.lines() {
+        let module_weight = module.expect("Something went wrong!").parse::<i32>().unwrap();
+        total_fuel_required += calc_fuel_for(module_weight);
     }
 
     println!("{}", total_fuel_required);
+}
+
+fn calc_fuel_for(weight: i32) -> i64 {
+    let mut complete = false;
+    let mut total_fuel_required = 0;
+    let mut remaining_weight: i64 = weight as i64;
+
+    while complete == false {
+        let fuel_required = ((remaining_weight / 3) as i64) - 2;
+        let capped_fuel_required = cmp::max(fuel_required, 0);
+        remaining_weight = capped_fuel_required;
+
+        if capped_fuel_required > 0 {
+            total_fuel_required += capped_fuel_required;
+        }
+        
+        if remaining_weight == 0 {
+            complete = true;
+        }
+    }
+
+    total_fuel_required
 }
