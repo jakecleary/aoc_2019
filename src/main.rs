6cb3e8b78ab10999cc94361a45d6ab8e080489de
Jake Cleary <shout@jakecleary.net>
2019-12-02 14:16:27 +0000

    Solve day 1 part 1 :star:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("input.txt").expect("Something went wrong!");
+    let reader = BufReader::new(file);
+
+    let mut total_fuel_required = 0;
+
+    for line in reader.lines() {
+        let weight = line.expect("Something went wrong!").parse::<i32>().unwrap();
+        let fuel_required = ((weight / 3) as i64) - 2;
+        total_fuel_required += fuel_required;
+    }
+
+    println!("{}", total_fuel_required);
 }
